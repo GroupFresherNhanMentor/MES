104ef19bf27233ccaed95255a5b6db0c456ae864
Pham The Manh <pmanhh19@gmail.com>
2026-07-29 22:03:32 +0700

    add find by Ids product

--- be/src/main/java/fpt/qn/mes/master/product/domain/repository/ProductRepository.java
+++ be/src/main/java/fpt/qn/mes/master/product/domain/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package fpt.qn.mes.master.product.domain.repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -8,6 +10,7 @@ import fpt.qn.mes.master.product.domain.entities.Product;
 
 public interface ProductRepository {
     Optional<Product> findById(UUID id);
+    List<Product> findByIds(Collection<UUID> ids);
     Product save(Product product);
     Product update(Product product);
     void deleteById(UUID id);
